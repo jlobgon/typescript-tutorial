1474da4ae322e7771f0adfcb8820be9a7ad1ab39
jlobgon <jlobgon@iescarrillo.es>
2025-09-29 08:47:42 +0200

    Añadido ejemplos de la sintaxis básica, modificación del gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,51 @@
-let x = 1
-console.log("Hola2");
+
+
+//Declaración de variables
+const z = "Fran"
+if(true){
+    var x = "Maria"
+    let y = "Jose"; // Tiene un ámbito de bloque
+}
+x = "Jose" // x tiene un ámbito global por eso puede modificar el valor asignado del if
+
+console.log(x)
+
+// TIPOS DE DATOS
+/**
+ * Datos Primitivos
+ * string
+ * number
+ * boolean
+ */
+
+let nombre:string = "Jose Antonio"
+let edad:number = 18;
+let mayor_edad:boolean = true
+
+/** Tipos de datos especiales
+ * 
+ * any
+ * unknown
+ * void
+ */
+
+//Any provocaria un error en tiempo de compliación
+let cualquierCosa:any = "Hola"
+cualquierCosa = 2
+cualquierCosa = true;
+
+//console.log(cualquierCosa.toLowerCase())
+
+//Uso del tipo unknown
+let tipoDeDatoDesconocido:unknown;
+
+tipoDeDatoDesconocido = "Nombre"
+tipoDeDatoDesconocido = 18
+
+if (typeof(tipoDeDatoDesconocido) == "string"){
+    console.log(tipoDeDatoDesconocido.toLocaleLowerCase())
+}
+
+
+//CONCATENAR
+console.log(`Tu nombre es ${x}`)
